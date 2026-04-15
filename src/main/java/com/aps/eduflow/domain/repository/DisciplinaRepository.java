e5a61541b68f5529861c9cac1ff9324b588f73e2
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-04-15 12:37:21 -0300

    feat: Gestão de Disciplinas: Cadastro e Manutenção de Matérias (close #6)

--- src/main/java/com/aps/eduflow/domain/repository/DisciplinaRepository.java
+++ src/main/java/com/aps/eduflow/domain/repository/DisciplinaRepository.java
@@ -3,6 +3,7 @@ package com.aps.eduflow.domain.repository;
 import com.aps.eduflow.domain.entity.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
@@ -10,4 +11,12 @@ public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
     Optional<Disciplina> findByCodigo(String codigo);
 
     boolean existsByCodigo(String codigo);
+
+    boolean existsByCodigoAndIdNot(String codigo, Long id);
+
+    List<Disciplina> findByAtivoTrueOrderByNomeAsc();
+
+    List<Disciplina> findByNomeContainingIgnoreCaseOrderByNomeAsc(String nome);
+
+    List<Disciplina> findByCodigoContainingIgnoreCaseOrderByNomeAsc(String codigo);
 }
