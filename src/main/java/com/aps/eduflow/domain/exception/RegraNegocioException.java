adfc10a9cd5e54df283224c42d2a01f7e6cc79ee
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-04-15 12:29:04 -0300

    feat: CRUD de Usuários: Gestão e Diferenciação de Perfis (close #5)

---
+++ src/main/java/com/aps/eduflow/domain/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package com.aps.eduflow.domain.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
